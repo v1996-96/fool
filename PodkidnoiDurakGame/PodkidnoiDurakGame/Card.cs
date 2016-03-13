ae0a6f5696d89633567fdd931138ac50f0842c7d
Yustus Polina <polayustus@styleru.net>
2016-03-13 12:17:54 +0300

    added card class

---
+++ PodkidnoiDurakGame/PodkidnoiDurakGame/Card.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodkidnoiDurakGame
+{
+    class Card : Sprite
+    {
+        public override Vector2 direction { set; get; }
+
+        public event Action<Card> OnClick;
+        public event Action<Card> OnHover;
+
+
+        public Card(Texture2D textureImage, Vector2 position, Point frameSize, Point currentFrame, float zIndex)
+            :base(textureImage, position, frameSize, currentFrame, zIndex){}
+
+        public Card(Texture2D textureImage, Vector2 position, Point frameSize, Point currentFrame, float zIndex, float rotation, Vector2 rotationOrigin)
+            : base(textureImage, position, frameSize, currentFrame, zIndex, rotation, rotationOrigin) { }
+
+        public override void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            MouseState mouseState = Mouse.GetState();
+            if (rectangle.Contains(mouseState.Position) &&
+                OnHover != null)
+            {
+                OnHover(this);
+
+                if (mouseState.LeftButton == ButtonState.Pressed &&
+                    OnClick != null)
+                    OnClick(this);
+            }
+
+            base.Update(gameTime, clientBounds);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            base.Draw(gameTime, spriteBatch);
+        }
+    }
+}
