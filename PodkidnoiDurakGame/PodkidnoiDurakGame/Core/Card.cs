dc78fbe52e53709e5a5071116fd3376e12c805ef
Yustus Polina <polayustus@styleru.net>
2016-03-13 13:03:49 +0300

    card struct

---
+++ PodkidnoiDurakGame/PodkidnoiDurakGame/Core/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodkidnoiDurakGame.Core
+{
+    struct Card
+    {
+        public CardType CardType { get; set; }
+
+        public CardSuit CardSuit { get; set; }
+
+        public bool IsTrump { get; set; }
+
+        public bool CanCover(Card covering)
+        {
+            if (covering.IsTrump && !this.IsTrump)
+                return true;
+
+            if (!covering.IsTrump && this.IsTrump)
+                return false;
+
+            if ((covering.IsTrump && this.IsTrump) ||
+                (!covering.IsTrump && !this.IsTrump))
+                return covering.CardType > this.CardType;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(Card obj)
+        {
+            return (CardType == obj.CardType) && (CardSuit == obj.CardSuit);
+        }
+    }
+}
